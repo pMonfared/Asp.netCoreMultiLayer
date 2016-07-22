5e99b78c399095bdd781b7c5d08294224f771b8b
Poria Monfared <poriya.monfared@gmail.com>
2016-07-22 17:36:14 +0430

    First Commit

---
+++ src/SampleFive.Di/StructureMapContainer.cs
@@ -0,0 +1,7 @@
+﻿namespace SampleFive.Di
+{
+    public class StructureMapContainer
+    {
+         
+    }
+}
---
+++ src/SampleFive.ServiceLayer/IMessagesService.cs
@@ -0,0 +1,13 @@
+﻿using SampleFive.PresentaionLayer;
+
+namespace SampleFive.ServiceLayer
+{
+    public interface IMessagesService
+    {
+        string GetSiteName();
+        string GetSiteName2();
+        string GetSiteName3();
+        string GetSiteWelcome(string title = "my Kingdom");
+        SiteCopyRightConfig GetSiteCopyRightConfig();
+    }
+}
